33cba9a0905a5b29027d65976804c971c2955809
Dastanus <nixe141417@gmail.com>
2019-02-25 22:40:26 +0300

    feat: realize function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zeros = Infinity;
+  let points = [];
+  for (j=2;j<base;){
+      if(base%j==0){
+        points.push(j);
+        base/=j;
+        }
+      else {
+        j++;
+        }
+    }
+  
+  points.push(base);
+  points.reverse();
+  while(points[0]){
+    let currentBase = points[0];
+    let count = 0;
+    let l = points.length;
+    let currentZeros = 0;
+    for(i=0;i<l;i++){
+      if(points[0]==currentBase){
+        count++;
+        points.splice(0,1);
+        l--;
+        i--;
+      }  
+    }
+    currentNumber = number;
+    while(Math.floor(currentNumber)>0){
+      currentNumber/=currentBase;
+      currentZeros+= Math.floor(currentNumber);
+    }
+    currentZeros/=count;
+    if(currentZeros<zeros){
+      zeros=Math.floor(currentZeros);
+    }
+    
+  }
+  return zeros;
 }
